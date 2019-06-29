0e15b3d313d903667f60bc1bea7196466a8fb8a8
admin <admin>
2019-06-29 21:44:14 +0800

    添加分支

---
+++ src/cn/hui/domain/User.java
@@ -0,0 +1,39 @@
+package cn.hui.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: 666
+ * @Date: 2019/6/29
+ * @Time: 21:20
+ */
+
+public class User {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
